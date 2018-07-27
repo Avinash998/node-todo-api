55c7d0335abf3ea5c79cab663924afc5f72f8531
Avinash <avinashgolu.chourasia@gmail.com>
2018-07-27 23:34:46 +0530

    mongoose query example added

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,48 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+/*var id = '5b5b4dec665ccd7c0e870a18';
+
+if(!ObjectID.isValid(id)){
+	console.log('Id not valid');
+}*/
+
+
+/*Todo.find({
+	_id: id
+}).then((todos) => {
+	console.log('Todos ',todos);
+});
+
+Todo.findOne({
+	_id: id
+}).then((todo) => {
+	console.log('Todo ',todo);
+});
+*/
+/*Todo.findById(id).then((todo) => {
+
+	if(!todo){
+		return console.log('Id not found');
+	}
+	console.log('Todo_FindById ',todo);
+}).catch((e) => console.log(e));
+*/
+
+//User FindById
+
+var id = '5b59f403c68dba2023699e44';
+
+if(!ObjectID.isValid(id)){
+	console.log('User ID is not valid');
+}
+
+User.findById(id).then((user) => {
+	if(!user){
+		return console.log('User id doesn\'t Exists');
+	}
+	console.log('User_findById ',user);
+}).catch((e) => console.log(e));
