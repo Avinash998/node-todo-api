7829620e853516898529f0921b95132150000a46
Avinash <avinashgolu.chourasia@gmail.com>
2018-07-28 07:48:43 +0530

    Setting mlab Database connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://dudesum:dudesum998@ds257851.mlab.com:57851/todo-app'||'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGOLAB_URI||'mongodb://localhost:27017/TodoApp');
 
 
 module.exports = {
