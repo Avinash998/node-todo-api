39d7070cd0a9b6c450a6594bd8a6a1107c69ca5d
Avinash <avinashgolu.chourasia@gmail.com>
2018-07-28 06:15:58 +0530

    Database connection established mlab.com

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://Avinash998:<Avinash@98>@ds257851.mlab.com:57851/todo-app'||'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://dudesum:dudesum998@ds257851.mlab.com:57851/todo-app'||'mongodb://localhost:27017/TodoApp');
 
 
 module.exports = {
