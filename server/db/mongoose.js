4e901b8c8586b623fb0a4e68e95b3dd8ccebb9dd
Avinash <avinashgolu.chourasia@gmail.com>
2018-07-30 02:05:32 +0530

    A

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -11,7 +11,7 @@ var options = {
   reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
   reconnectInterval: 500, // Reconnect every 500ms
   poolSize: 10, // Maintain up to 10 socket connections
-  // If not connected, return errors immediately rather than waiting for reconnect
+  // If not connected, return erros immediately rather than waiting for reconnect
   bufferMaxEntries: 0
 };
 mongoose.connect(process.env.PROD_MONGODB, options);
