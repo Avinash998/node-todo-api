e65e4e392d98aba275b1e3f52d4a30d9dd2f0952
Avinash <avinashgolu.chourasia@gmail.com>
2018-07-30 20:12:58 +0530

    Final DB configure

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,22 +4,6 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGOLAB_URI||'mongodb://localhost:27017/TodoApp');
 
 
-/*mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://dudesum:dudesum998@ds257851.mlab.com:57851/todo-app');*/
-
-/*mongoose.Promise = global.Promise;
-var options = {
-  useMongoClient: true,
-  autoIndex: false, // Don't build indexes
-  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-  reconnectInterval: 500, // Reconnect every 500ms
-  poolSize: 10, // Maintain up to 10 socket connections
-  // If not connected, return erros immediately rather than waiting for reconnect
-  bufferMaxEntries: 0
-};
-mongoose.connect('mongodb://dudesum:dudesum998@ds257851.mlab.com:57851/todo-app', options);
-*/
-
 module.exports = {
 	mongoose
 };
