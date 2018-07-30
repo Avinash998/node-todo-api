fc7d2e7e35441f4a0997aa7493e6174c3fef4d89
Avinash <avinashgolu.chourasia@gmail.com>
2018-07-31 02:27:26 +0530

    PATCH /todos/:id added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // var mongoose = require('mongoose');
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -66,6 +67,32 @@ app.delete('/todos/:id', (req, res, next) => {
 	}).catch((e) => {
 		res.status(400).send(e);
 	});
+});
+
+app.patch('/todos/:id',(req, res, next) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text','completed']);
+
+	if(!ObjectID.isValid(id)){
+		res.status(404).send('Invalid Id');
+	}
+	if(_.isBoolean(body.completed) && body.completed){
+		body.completedAt = new Date().getTime();
+	}
+	else{
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id , {$set: body}, {new: true}).then((todo) => {
+		if(!todo){
+			res.status(404).send("Id doesn't Exists");
+		}
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send(e);
+	});
+
 });
 
 app.listen(port, () => {
