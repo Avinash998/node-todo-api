d8f26dda225559e40c03251a8656beff24d623b5
Avinash <avinashgolu.chourasia@gmail.com>
2018-07-30 17:12:36 +0530

    Db reconfigure

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,8 +4,11 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://dudesum:dudesum998@ds257851.mlab.com:57851/todo-app'||process.env.MONGOLAB_URI||'mongodb://localhost:27017/TodoApp');
 */
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://dudesum:dudesum998@ds257851.mlab.com:57851/todo-app');
 
-var options = {
+
+/*var options = {
   useMongoClient: true,
   autoIndex: false, // Don't build indexes
   reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
@@ -14,7 +17,7 @@ var options = {
   // If not connected, return erros immediately rather than waiting for reconnect
   bufferMaxEntries: 0
 };
-mongoose.connect(/*process.env.PROD_MONGODB*/'mongodb://dudesum:dudesum998@ds257851.mlab.com:57851/todo-app', options);
+mongoose.connect('mongodb://dudesum:dudesum998@ds257851.mlab.com:57851/todo-app', options);*/
 
 
 module.exports = {
