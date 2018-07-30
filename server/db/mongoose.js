cf666329cc23e25fbff6399b94319fe63f768931
Avinash <avinashgolu.chourasia@gmail.com>
2018-07-30 19:28:14 +0530

    A

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,11 +4,11 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://dudesum:dudesum998@ds257851.mlab.com:57851/todo-app'||process.env.MONGOLAB_URI||'mongodb://localhost:27017/TodoApp');
 */
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://dudesum:dudesum998@ds257851.mlab.com:57851/todo-app');
+/*mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://dudesum:dudesum998@ds257851.mlab.com:57851/todo-app');*/
 
 
-/*var options = {
+var options = {
   useMongoClient: true,
   autoIndex: false, // Don't build indexes
   reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
@@ -17,7 +17,7 @@ mongoose.connect('mongodb://dudesum:dudesum998@ds257851.mlab.com:57851/todo-app'
   // If not connected, return erros immediately rather than waiting for reconnect
   bufferMaxEntries: 0
 };
-mongoose.connect('mongodb://dudesum:dudesum998@ds257851.mlab.com:57851/todo-app', options);*/
+mongoose.connect('mongodb://dudesum:dudesum998@ds257851.mlab.com:57851/todo-app', options);
 
 
 module.exports = {
