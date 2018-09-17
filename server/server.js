eb66630be100688c97352831719f9b79c46a07e3
Avinash <avinashgolu.chourasia@gmail.com>
2018-09-17 11:20:02 +0530

    Add GET /users/me and authenticate user

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 console.log('Ready for the routes--');
 
@@ -111,6 +112,11 @@ app.post('/users', (req,res,next) => {
 	}).catch((e) => {
 		res.status(400).send(e);
 	});
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+	res.send(req.user);
 });
 
 app.listen(port, () => {
