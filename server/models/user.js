3c7d85a351c233bbe4294a713af962403a3087cc
Avinash <avinashgolu.chourasia@gmail.com>
2018-09-22 02:36:34 +0530

    Adding DELETE users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -53,6 +53,16 @@ UserSchema.methods.generateAuthToken = function () {
 	});
 };
 
+UserSchema.methods.removeToken = function (token) {
+	var user = this;
+	console.log("inside removeToken");
+	return user.update({
+		$pull: {
+			tokens: {token}
+		}
+	});
+};
+
 UserSchema.statics.findByToken = function (token) {
 	var User = this;
 	var decoded ;
